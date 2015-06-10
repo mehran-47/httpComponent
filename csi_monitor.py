232cc1c5d08e0fc220f5ff1a1338ed199acda173
root <root@flap-vnode6.localdomain>
2015-06-10 10:09:00 -0400

    fixed killing dead processes in csi_monitor

--- csi_monitor.py
+++ csi_monitor.py
@@ -9,10 +9,13 @@ if __name__ == '__main__':
         print('CSI monitor is already running')
         with open('/opt/SA_stats/csi_monitor') as cm: pidTokill = str(cm.read()).rsplit('\n')[0]
         if os.path.exists('/proc/'+pidTokill): call(['kill', '-9', pidTokill])
+        with open('/opt/SA_stats/csi_monitor', 'w') as csiMf: csiMf.write(str(os.getpid()))
     component_name = str(os.environ.get('SA_AMF_COMPONENT_NAME'))
-    with open('/tmp/'+component_name+'.pid', 'r') as compPIDf : component_pid = str(compPIDf.readline())
+    if os.path.exists('/tmp/'+component_name+'.pid'):
+        with open('/tmp/'+component_name+'.pid', 'r') as compPIDf : component_pid = str(compPIDf.readline())
+    else:
+        return None
     with open('/opt/SA_stats/pid', 'r') as pidf: http_pid = str(pidf.readline())
-    with open('/opt/SA_stats/csi_monitor', 'w') as csiMf: csiMf.write(str(os.getpid()))
     component_pid = component_pid.rsplit('\n')[0]
     http_pid = http_pid.rsplit('\n')[0]
     try:
